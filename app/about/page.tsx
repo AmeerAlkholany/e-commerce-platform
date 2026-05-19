0669e7ef35045d2db1548085f0d0937cad097ca2
Ahmed Alwakidi <ahmedcomahmedocm@gmail.com>
2026-05-19 14:05:36 +0300

    ADD NEW SHIT

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>This stupid page is about this stupid system</div>
-  )
-}
+  return <div>This stupid page is about this stupid system oifdah;gHEGA</div>;
+};
 
-export default About
+export default About;
